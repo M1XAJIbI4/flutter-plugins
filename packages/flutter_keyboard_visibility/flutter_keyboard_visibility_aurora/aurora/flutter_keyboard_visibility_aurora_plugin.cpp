9817860c905203fed91e1cd5295959c4fb4eea22
Vitaliy Zarubin <v.zarubin@omp.ru>
2024-03-11 13:43:49 +0300

    [flutter_keyboard_visibility_aurora][change] Use flutter interface client_wrapper.

--- packages/flutter_keyboard_visibility/flutter_keyboard_visibility_aurora/aurora/flutter_keyboard_visibility_aurora_plugin.cpp
+++ packages/flutter_keyboard_visibility/flutter_keyboard_visibility_aurora/aurora/flutter_keyboard_visibility_aurora_plugin.cpp
@@ -3,85 +3,107 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 #include <flutter_keyboard_visibility_aurora/flutter_keyboard_visibility_aurora_plugin.h>
-#include <flutter/method-channel.h>
 #include <flutter/platform-events.h>
 #include <flutter/platform-methods.h>
-#include <thread>
 
-FlutterKeyboardVisibilityAuroraPlugin::FlutterKeyboardVisibilityAuroraPlugin()
+namespace Channels {
+    constexpr auto EventVisibility = "flutter_keyboard_visibility_aurora_state";
+    constexpr auto EventHeight = "flutter_keyboard_visibility_aurora_height";
+    constexpr auto Methods = "flutter_keyboard_visibility_aurora";
+} // namespace Channels
+
+namespace Methods {
+    constexpr auto KeyboardHeight = "getKeyboardHeight";
+} // namespace Methods
+
+void FlutterKeyboardVisibilityAuroraPlugin::RegisterWithRegistrar(PluginRegistrar* registrar)
 {
-  PlatformEvents::SubscribeKeyboardVisibilityChanged(
-      [this](bool state)
-      {
-        if (this->m_sendEventVisibility)
-        {
-          EventChannelAurora("flutter_keyboard_visibility_aurora_state", MethodCodecType::Standard)
-              .SendEvent(state);
+    // Create plugin
+    std::unique_ptr<FlutterKeyboardVisibilityAuroraPlugin> plugin(new FlutterKeyboardVisibilityAuroraPlugin(
+        std::move(std::make_unique<MethodChannel>(
+        registrar->messenger(), Channels::Methods,
+        &flutter::StandardMethodCodec::GetInstance())),
+        std::move(std::make_unique<EventChannel>(
+        registrar->messenger(), Channels::EventVisibility,
+        &flutter::StandardMethodCodec::GetInstance())),
+        std::move(std::make_unique<EventChannel>(
+        registrar->messenger(), Channels::EventHeight,
+        &flutter::StandardMethodCodec::GetInstance()))
+    ));
+
+    // Register plugin
+    registrar->AddPlugin(std::move(plugin));
+}
+
+FlutterKeyboardVisibilityAuroraPlugin::FlutterKeyboardVisibilityAuroraPlugin(
+    std::unique_ptr<MethodChannel> methodChannel,
+    std::unique_ptr<EventChannel> eventChannelVisibility,
+    std::unique_ptr<EventChannel> evenChannelHeight
+) : m_methodChannel(std::move(methodChannel)),
+    m_eventChannelVisibility(std::move(eventChannelVisibility)),
+    m_evenChannelHeight(std::move(evenChannelHeight))
+{
+    // Create MethodHandler
+    RegisterMethodHandler();
+
+    // Create StreamHandler
+    RegisterStreamHandler();
+
+    // Listen change state keyboard
+    PlatformEvents::SubscribeKeyboardVisibilityChanged([&](bool state) {
+      if (m_stateEventChannelVisibility) {
+        m_sinkVisibility->Success(state);
+      }
+      if (m_stateEventChannelHeight) {
+        m_sinkHeight->Success(PlatformMethods::GetKeyboardHeight());
+      }
+    });
+}
+
+void FlutterKeyboardVisibilityAuroraPlugin::RegisterMethodHandler()
+{
+    m_methodChannel->SetMethodCallHandler(
+        [&](const MethodCall& call, std::unique_ptr<MethodResult> result) {
+            if (call.method_name().compare(Methods::KeyboardHeight) == 0) {
+                result->Success(PlatformMethods::GetKeyboardHeight());
+            }
+            else {
+                result->Success();
+            }
+        });
+}
+
+void FlutterKeyboardVisibilityAuroraPlugin::RegisterStreamHandler()
+{
+    // Event Visibility
+    auto handlerVisibility = std::make_unique<flutter::StreamHandlerFunctions<EncodableValue>>(
+        [&](const EncodableValue* arguments,
+            std::unique_ptr<flutter::EventSink<EncodableValue>>&& events
+        ) -> std::unique_ptr<flutter::StreamHandlerError<EncodableValue>> {
+            m_sinkVisibility = std::move(events);
+            m_stateEventChannelVisibility = true;
+            return nullptr;
+        },
+        [&](const EncodableValue* arguments) -> std::unique_ptr<flutter::StreamHandlerError<EncodableValue>> {
+            m_stateEventChannelVisibility = false;
+            return nullptr;
         }
+    );
+    m_eventChannelVisibility->SetStreamHandler(std::move(handlerVisibility));
 
-        if (this->m_sendEventHeight)
-        {
-          EventChannelAurora("flutter_keyboard_visibility_aurora_height", MethodCodecType::Standard)
-              .SendEvent(PlatformMethods::GetKeyboardHeight());
+    // Event Height
+    auto handlerHeight = std::make_unique<flutter::StreamHandlerFunctions<EncodableValue>>(
+        [&](const EncodableValue* arguments,
+            std::unique_ptr<flutter::EventSink<EncodableValue>>&& events
+        ) -> std::unique_ptr<flutter::StreamHandlerError<EncodableValue>> {
+            m_sinkHeight = std::move(events);
+            m_stateEventChannelHeight = true;
+            return nullptr;
+        },
+        [&](const EncodableValue* arguments) -> std::unique_ptr<flutter::StreamHandlerError<EncodableValue>> {
+            m_stateEventChannelHeight = false;
+            return nullptr;
         }
-      });
-}
-
-void FlutterKeyboardVisibilityAuroraPlugin::RegisterWithRegistrar(PluginRegistrarAurora &registrar)
-{
-  registrar.RegisterMethodChannel("flutter_keyboard_visibility_aurora",
-                                  MethodCodecType::Standard,
-                                  [this](const MethodCallAurora &call)
-                                  { this->onMethodCall(call); });
-
-  registrar.RegisterEventChannel(
-      "flutter_keyboard_visibility_aurora_state",
-      MethodCodecType::Standard,
-      [this](const Encodable &)
-      {
-        this->m_sendEventVisibility = true;
-        return EventResponse();
-      },
-      [this](const Encodable &)
-      {
-        this->m_sendEventVisibility = false;
-        return EventResponse();
-      });
-
-  registrar.RegisterEventChannel(
-      "flutter_keyboard_visibility_aurora_height",
-      MethodCodecType::Standard,
-      [this](const Encodable &)
-      {
-        this->m_sendEventHeight = true;
-        return EventResponse();
-      },
-      [this](const Encodable &)
-      {
-        this->m_sendEventHeight = false;
-        return EventResponse();
-      });
-}
-
-void FlutterKeyboardVisibilityAuroraPlugin::onMethodCall(const MethodCallAurora &call)
-{
-  const auto &method = call.GetMethod();
-
-  if (method == "getKeyboardHeight")
-  {
-    onGetKeyboardHeight(call);
-    return;
-  }
-
-  unimplemented(call);
-}
-
-void FlutterKeyboardVisibilityAuroraPlugin::onGetKeyboardHeight(const MethodCallAurora &call)
-{
-  call.SendSuccessResponse(PlatformMethods::GetKeyboardHeight());
-}
-
-void FlutterKeyboardVisibilityAuroraPlugin::unimplemented(const MethodCallAurora &call)
-{
-  call.SendSuccessResponse(nullptr);
+    );
+    m_evenChannelHeight->SetStreamHandler(std::move(handlerHeight));
 }
--- packages/flutter_keyboard_visibility/flutter_keyboard_visibility_aurora/aurora/include/flutter_keyboard_visibility_aurora/flutter_keyboard_visibility_aurora_plugin.h
+++ packages/flutter_keyboard_visibility/flutter_keyboard_visibility_aurora/aurora/include/flutter_keyboard_visibility_aurora/flutter_keyboard_visibility_aurora_plugin.h
@@ -5,22 +5,50 @@
 #ifndef FLUTTER_PLUGIN_FLUTTER_KEYBOARD_VISIBILITY_AURORA_PLUGIN_H
 #define FLUTTER_PLUGIN_FLUTTER_KEYBOARD_VISIBILITY_AURORA_PLUGIN_H
 
-#include <flutter/plugin-interface.h>
 #include <flutter_keyboard_visibility_aurora/globals.h>
 
-class PLUGIN_EXPORT FlutterKeyboardVisibilityAuroraPlugin final : public PluginInterface
+#include <flutter/plugin_registrar.h>
+#include <flutter/method_channel.h>
+#include <flutter/event_channel.h>
+#include <flutter/encodable_value.h>
+#include <flutter/standard_method_codec.h>
+#include <flutter/event_stream_handler_functions.h>
+
+typedef flutter::Plugin Plugin;
+typedef flutter::PluginRegistrar PluginRegistrar;
+typedef flutter::EncodableValue EncodableValue;
+typedef flutter::MethodChannel<EncodableValue> MethodChannel;
+typedef flutter::MethodCall<EncodableValue> MethodCall;
+typedef flutter::MethodResult<EncodableValue> MethodResult;
+typedef flutter::EventChannel<EncodableValue> EventChannel;
+typedef flutter::EventSink<EncodableValue> EventSink;
+
+class PLUGIN_EXPORT FlutterKeyboardVisibilityAuroraPlugin final : public flutter::Plugin
 {
 public:
-    FlutterKeyboardVisibilityAuroraPlugin();
-    void RegisterWithRegistrar(PluginRegistrarAurora &registrar) override;
+    static void RegisterWithRegistrar(PluginRegistrar* registrar);
 
 private:
-    bool m_sendEventVisibility = false;
-    bool m_sendEventHeight = false;
+    // Creates a plugin that communicates on the given channel.
+    FlutterKeyboardVisibilityAuroraPlugin(
+        std::unique_ptr<MethodChannel> methodChannel,
+        std::unique_ptr<EventChannel> eventChannelVisibility,
+        std::unique_ptr<EventChannel> evenChannelHeight
+    );
 
-    void onMethodCall(const MethodCallAurora &call);
-    void onGetKeyboardHeight(const MethodCallAurora &call);
-    void unimplemented(const MethodCallAurora &call);
+    // Methods register handlers channels
+    void RegisterMethodHandler();
+    void RegisterStreamHandler();
+
+    std::unique_ptr<MethodChannel> m_methodChannel;
+    std::unique_ptr<EventChannel> m_eventChannelVisibility;
+    std::unique_ptr<EventChannel> m_evenChannelHeight;
+
+    bool m_stateEventChannelVisibility = false;
+    bool m_stateEventChannelHeight = false;
+
+    std::unique_ptr<EventSink> m_sinkVisibility;
+    std::unique_ptr<EventSink> m_sinkHeight;
 };
 
 #endif /* FLUTTER_PLUGIN_FLUTTER_KEYBOARD_VISIBILITY_AURORA_PLUGIN_H */
